b208dbfeeff94714975678c43bf62e6e1c23f653
Edvinas <edvinasstanys@gmail.com>
2021-06-22 08:59:35 +0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MainForm from './components/MainForm'
+
+function App() {
+  return (
+    <div className="App">
+      <MainForm/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/MainForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MainForm extends Component {
+  render() {
+    return (
+      <div className='main-form'>
+         <h1>Main form</h1>
+      </div>
+    )
+  }
+}
+
